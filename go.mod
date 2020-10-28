8a01cdf522ffb2d706a5bb44d6bd60c061d5b686
tonyquang <quang.bui@s3corp.com.vn>
2020-10-28 17:28:25 +0700

    Write More Some Func

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module friends_management_v2
 
 go 1.15
 
-require gorm.io/gorm v1.20.5
+require (
+	gorm.io/driver/postgres v1.0.5
+	gorm.io/gorm v1.20.5
+)
---
+++ utils/connection_db.go
@@ -0,0 +1,29 @@
+package utils
+
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "postgres"
+	dbname   = "FriendsManagement"
+)
+
+func CreateConnection() *gorm.DB {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := gorm.Open(postgres.Open(psqlInfo), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Connect to database successfully")
+	return db
+}
