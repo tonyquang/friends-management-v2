a1453cb1b5cf514de89c161eb9c0c8980c672827
tonyquang <quangbui14041999@gmail.com>
2020-11-25 17:34:53 +0700

    JWT

---
+++ controller/login.go
@@ -0,0 +1,44 @@
+package controller
+
+import (
+	service "friends_management_v2/services"
+
+	"github.com/gin-gonic/gin"
+)
+
+type LoginCredentials struct {
+	Email    string `form:"email"`
+	Password string `form:"password"`
+}
+
+//login contorller interface
+type LoginController interface {
+	Login(ctx *gin.Context) string
+}
+
+type loginController struct {
+	loginService service.LoginService
+	jWtService   service.JWTService
+}
+
+func LoginHandler(loginService service.LoginService,
+	jWtService service.JWTService) LoginController {
+	return &loginController{
+		loginService: loginService,
+		jWtService:   jWtService,
+	}
+}
+
+func (controller *loginController) Login(ctx *gin.Context) string {
+	var credential LoginCredentials
+	err := ctx.ShouldBind(&credential)
+	if err != nil {
+		return "no data found"
+	}
+	isUserAuthenticated := controller.loginService.LoginUser(credential.Email, credential.Password)
+	if isUserAuthenticated {
+		return controller.jWtService.GenerateToken(credential.Email, true)
+
+	}
+	return ""
+}
--- controller/routes.go
+++ controller/routes.go
@@ -1,14 +1,17 @@
 package controller
 
 import (
+	"fmt"
 	"net/http"
 
 	friendshipController "friends_management_v2/controller/friendship"
 	userController "friends_management_v2/controller/user"
 	migration "friends_management_v2/migrations"
+	service "friends_management_v2/services"
 	friendshipService "friends_management_v2/services/friendship"
 	userService "friends_management_v2/services/user"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -21,10 +24,26 @@ import (
 func Setup(db *gorm.DB) http.Handler {
 	friendshipService := friendshipService.NewFriendshipManager(db)
 	userService := userService.NewUserManager(db)
+
+	var loginService service.LoginService = service.StaticLoginService()
+	var jwtService service.JWTService = service.JWTAuthService()
+	var loginController LoginController = LoginHandler(loginService, jwtService)
+
 	migration.InitMigration(db)
 	gin.SetMode(gin.TestMode)
 
 	r := gin.Default()
+
+	r.POST("/login", func(ctx *gin.Context) {
+		token := loginController.Login(ctx)
+		if token != "" {
+			ctx.JSON(http.StatusOK, gin.H{
+				"token": token,
+			})
+		} else {
+			ctx.JSON(http.StatusUnauthorized, nil)
+		}
+	})
 
 	//url := ginSwagger.URL("http://localhost:3000/docs/swagger.json")
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
@@ -61,4 +80,21 @@ func Setup(db *gorm.DB) http.Handler {
 		friendshipController.GetUsersReceiveUpdateController(c, friendshipService)
 	})
 	return r
+}
+
+func AuthorizeJWT() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		const BEARER_SCHEMA = "Bearer"
+		authHeader := c.GetHeader("Authorization")
+		tokenString := authHeader[len(BEARER_SCHEMA):]
+		token, err := service.JWTAuthService().ValidateToken(tokenString)
+		if token.Valid {
+			claims := token.Claims.(jwt.MapClaims)
+			fmt.Println(claims)
+		} else {
+			fmt.Println(err)
+			c.AbortWithStatus(http.StatusUnauthorized)
+		}
+
+	}
 }
